7813bd9d2716a288f955b5151f2a990a52f55fad
Vu Phan <vuphan9@gmail.com>
2021-06-17 21:37:17 +0700

    Second commit with update fourth Banner title

--- src/main/java/PageObject/Travel.java
+++ src/main/java/PageObject/Travel.java
@@ -11,7 +11,7 @@ public class Travel {
     public FindElement firstBannerTitle = new FindElement(How.XPATH, "//h3[contains(.,'Homepage Front-End')]");
     public FindElement secondBannerTitle = new FindElement(How.XPATH, "//h3[contains(.,'Administrator Back-End')]");
     public FindElement thirdBannerTitle = new FindElement(How.XPATH, "//h3[contains(.,'Supplier Back-End')]");
-    public FindElement fourthBannerTitle = new FindElement(How.XPATH, "//section[@class='grey-box']//div[6]//a[contains(.,'Mobile Apps')]");
+    public FindElement fourthBannerTitle = new FindElement(How.XPATH, "//h3[contains(.,'iOS App')]");
 
 
     public void getPageTitle(){
